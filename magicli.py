2c0c9fa52d744b2353e1d154fa3f67931dd3acad
Patrick Elmer <patrick@elmer.ws>
2026-02-13 22:07:22 +0100

    Add --version to call function

--- magicli.py
+++ magicli.py
@@ -31,9 +31,9 @@ def magicli():
     name = name.replace("-", "_")
 
     if function := is_command(argv, module):
-        call(function, argv[1:], name)
+        call(function, argv[1:], module, name)
     elif inspect.isfunction(function := module.__dict__.get(name)):
-        call(function, argv)
+        call(function, argv, module)
     else:
         raise SystemExit(help_message(help_from_module, module))
 
@@ -53,7 +53,7 @@ def is_command(argv, module):
     return None
 
 
-def call(function, argv, name=None):
+def call(function, argv, module=None, name=None):
     """
     Converts arguments to function parameters and calls the function.
     Displays a help message if an exception occurs.
@@ -61,6 +61,13 @@ def call(function, argv, name=None):
     try:
         args, kwargs = args_and_kwargs(argv, function)
         function(*args, **kwargs)
+    except ValueError as error:
+        if "version" not in inspect.signature(function).parameters and (
+            (argv == ["--version"] and "--version" in get_docstring(function))
+            or (argv == ["-v"] and "-v, --version" in get_docstring(function))
+        ) and module:
+            raise SystemExit(get_version(module) or 0)
+        raise SystemExit(error.args[0])
     except Exception:
         raise SystemExit(help_message(help_from_function, function, name))
 
