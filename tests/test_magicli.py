f0aa5c2c38ed4b9a83a1e140c9f4e211121c43d6
Patrick Elmer <patrick@elmer.ws>
2026-02-23 08:25:22 +0100

    Simplify module patch to one function

--- tests/test_magicli.py
+++ tests/test_magicli.py
@@ -1,5 +1,6 @@
 import sys
 from unittest import mock
+from functools import partial
 
 import pytest
 
@@ -8,32 +9,40 @@ from magicli import magicli
 ANSWER = None
 
 
-def function_1():
+def name():
+    "--version"
     global ANSWER
     ANSWER = 1
 
 
-def function_2():
+def command():
+    "-v, --version"
     global ANSWER
     ANSWER = 2
 
 
-def _create_module(name):
+def create_module(name, version=None, functions=None):
     module = type(sys)(name)
-    module.name = function_1
-    module.command = function_2
     module.__doc__ = "docstring"
+    module.__version__ = version
+    for function in functions or []:
+        setattr(module, function.__name__, function)
     return module
 
 
-@mock.patch("importlib.import_module", side_effect=_create_module)
+module = partial(create_module, functions=[name, command])
+module_version = partial(module, version="1.2.3")
+module_empty = partial(create_module, functions=None)
+
+
+@mock.patch("importlib.import_module", side_effect=module)
 def test_module_imported(mocked):
     sys.argv = ["name"]
     magicli()
     mocked.assert_called_once_with("name")
 
 
-@mock.patch("importlib.import_module", side_effect=_create_module)
+@mock.patch("importlib.import_module", side_effect=module)
 def test_first_function_called(mocked):
     sys.argv = ["name"]
     magicli()
@@ -41,7 +50,7 @@ def test_first_function_called(mocked):
     assert ANSWER == 1
 
 
-@mock.patch("importlib.import_module", side_effect=_create_module)
+@mock.patch("importlib.import_module", side_effect=module)
 def test_command_called(mocked):
     sys.argv = ["name", "command"]
     magicli()
@@ -49,7 +58,7 @@ def test_command_called(mocked):
     assert ANSWER == 2
 
 
-@mock.patch("importlib.import_module", side_effect=_create_module)
+@mock.patch("importlib.import_module", side_effect=module)
 def test_wrong_command_not_called(mocked):
     sys.argv = ["name", "wrong_command"]
     with pytest.raises(SystemExit):
@@ -62,11 +71,7 @@ def test_empty_sys_argv():
         magicli()
 
 
-def _create_empty_module(name):
-    return type(sys)(name)
-
-
-@mock.patch("importlib.import_module", side_effect=_create_empty_module)
+@mock.patch("importlib.import_module", side_effect=module_empty)
 def test_module_without_functions(mocked):
     sys.argv = ["name"]
     with pytest.raises(SystemExit):
@@ -87,32 +92,14 @@ def test_module_is_magicli():
     assert error.value.code == 1
 
 
-def _create_module_with_docstring(name):
-    module = type(sys)(name)
-    module.name = lambda aa="", bb=None: None
-    module.name.__doc__ = "-a, --aa\n-b, --bb"
-    return module
-
-
-@mock.patch("importlib.import_module", side_effect=_create_module_with_docstring)
+@mock.patch("importlib.import_module", side_effect=module)
 def test_short_option_with_wrong_type(mocked):
     sys.argv = ["name", "-ab"]
     with pytest.raises(SystemExit):
         magicli()
 
 
-def _create_module_with_versions(name):
-    module = type(sys)(name)
-    module.name = function_1
-    module.name.__doc__ = "--version"
-    module.command = function_2
-    module.command.__doc__ = "-v, --version"
-    module.__doc__ = "docstring"
-    module.__version__ = "1.2.3"
-    return module
-
-
-@mock.patch("importlib.import_module", side_effect=_create_module_with_versions)
+@mock.patch("importlib.import_module", side_effect=module_version)
 def test_version(mocked, capsys):
     sys.argv = ["name", "--version"]
     with pytest.raises(SystemExit) as error:
@@ -126,7 +113,7 @@ def test_version(mocked, capsys):
     assert error.value.code == "-v: invalid short option"
 
 
-@mock.patch("importlib.import_module", side_effect=_create_module_with_versions)
+@mock.patch("importlib.import_module", side_effect=module_version)
 def test_version_with_command(mocked, capsys):
     sys.argv = ["name", "command", "-v"]
     with pytest.raises(SystemExit) as error:
